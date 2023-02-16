c51aa8726ef080d3439c6926edf9c3b92169c666
Nazar <nazarlviv1991@gmail.com>
2023-02-17 00:06:41 +0200

    Masti

--- karty.rb
+++ karty.rb
@@ -1,6 +1,6 @@
 
 # Comment dlya git pull
 karta = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'V', 'Q', 'K', 'A',]
-mast = ['Pika', 'Cherva', 'Hresta', 'Bubna']
+mast = ['♠', '♥', '♣', '♦']
 
 puts "#{karta.sample} #{mast.sample}"
